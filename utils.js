8e2628d630ff8aea86144093f7f08fb5e9d7ec38
NimbleArchitect <56797391+NimbleArchitect@users.noreply.github.com>
2020-05-31 21:37:05 +0100

    changed colours and added esc to clear

--- utils.js
+++ utils.js
@@ -6,32 +6,28 @@ let list = undefined;
 
 document.addEventListener('keyup', (e) => {
     if (e.keyCode === 27) {
-        closesnip();
+        let txt = document.getElementById('searchbox').value;
+        if (txt.length == 0) {
+            closesnip();
+        } else {
+            document.getElementById('searchbox').value = "";
+        }
     }
 });
 
 function addNodes(data) {
-	document.getElementById("myUL").innerHTML = "";
-	var ul = document.getElementById("myUL");
+    if (data.length <=0) return
+    
+    document.getElementById("myUL").innerHTML = "";
+    let ul = document.getElementById("myUL");
     let json = JSON.parse(data);
     if (json == null) return 
 	
 	for (var key in json) {
         let obj = json[key];
-        
-        var divtxt = document.createElement('div');
-        divtxt.id = "data";
-        divtxt.textContent = obj.name;
-        
-        var divhash = document.createElement('div');
-        divhash.id = "hash";
-        divhash.textContent = obj.hash;
 
-        var li = document.createElement("li");
-        li.appendChild(divtxt);
-        li.appendChild(divhash);
-        
-		ul.appendChild(li);
+        itm = buildsearchitem(obj.hash, obj.name);
+		ul.appendChild(itm);
     }
     
     listselect = document.querySelector('#myUL');
@@ -40,29 +36,57 @@ function addNodes(data) {
     currentlistlength = json.length;
 }
 
+function buildsearchitem(hash, name) {
+    var li = document.createElement("li");
+
+    var divtxt = document.createElement('div');
+    divtxt.id = "data";
+    divtxt.textContent = name;
+    li.appendChild(divtxt);
+    
+    var divhash = document.createElement('div');
+    divhash.id = "hash";
+    divhash.textContent = hash;
+    li.appendChild(divhash);
+    
+    return li;
+}
+
 function movelist(direction) {
     if (direction == 0) return;
     if (currentlistitem <= -2) return;
     
     prevlistitem = currentlistitem;
     currentlistitem += direction;
+    
+    boundpos = document.getElementById('searchcombo').getBoundingClientRect()
+    pos = list[currentlistitem].getBoundingClientRect();
 
     //rolled to far forward, set back to start
     if (currentlistitem >= (currentlistlength) ) {
         currentlistitem = -1;
+        document.getElementById('searchcombo').scrollTop = boundpos.top;
     }
     if (currentlistitem <= -2) {
         currentlistitem = currentlistlength - 1;
+        document.getElementById('searchcombo').scrollTop = boundpos.height;
+    }
+    
+    //goes down but not up
+    if (pos != undefined) {
+        if ((boundpos.height + boundpos.top) < (pos.height + pos.top)) {
+            list[prevlistitem].scrollIntoView();
+        }
     }
 
     if (currentlistitem == -1) {
-        list[prevlistitem].className = ""    
+        list[prevlistitem].className = "";
     }
 
     if (prevlistitem != -1) {
-        list[prevlistitem].className = ""
+        list[prevlistitem].className = "";
     }
-    list[currentlistitem].className = "selected"
+    list[currentlistitem].className = "selected";
 }
 
 function searchFor() {
@@ -86,7 +110,7 @@ function searchFor() {
             break;
             
         default:
-            let txt = document.getElementById('myInput').value;
+            let txt = document.getElementById('searchbox').value;
             searchsnip(txt).then(function(result) { 
                 return addNodes(result);
             })
@@ -97,8 +121,6 @@ function searchFor() {
 function typesnippet() {
     if (currentlistitem != -1) {
         hashid = list[currentlistitem].querySelector('#hash').innerText
-        //console.log( hashid );
-        //console.log( list[currentlistitem -1]
         writesnip(hashid)
     } else {
         //console.log("search");
@@ -110,5 +132,5 @@ function saveform() {
     let txtcode = document.getElementById('code').value;
 
     savesnip(txttitle, txtcode);
-    document.getElementById('addnew').style.display=''
+    document.getElementById('box-addnew').style.display=''
 }
