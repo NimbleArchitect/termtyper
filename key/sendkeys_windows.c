02912374f7f24b461d8afa77a5f3823d94dccbae
NimbleArchitect <56797391+NimbleArchitect@users.noreply.github.com>
2020-08-03 00:00:06 +0100

    windows autotype is now working

--- key/sendkeys_windows.c
+++ key/sendkeys_windows.c
@@ -1,56 +1,77 @@
 #include "sendkeys_windows.h"
 
-#include <unistd.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-
-#define WINVER 0x0500
-#include <windows.h>
-
-
-int Sendkey(const char *letter, int shift) {
-    int n = 0;
+int Sendkey(const char* letter) {
+    //if shift set wscan to MapVirtualKey( VK_LSHIFT, 0 );
     // Create an array of generic keyboard INPUT structures
-    INPUT ip[10];
-    for (n=0 ; n<10 ; ++n)
-    {
-        ip->type = INPUT_KEYBOARD;
-        ip->ki.wScan = 0;
-        ip->ki.time = 0;
-        ip->ki.dwFlags = 0; // 0 for key press
-        ip->ki.dwExtraInfo = 0;
+    int c = 0;
+    INPUT ip[4];
+    for (int n=0; n<4; ++n) {
+        ip[n].type = INPUT_KEYBOARD;
+        ip[n].ki.wScan = 0;
+        ip[n].ki.wVk = 0;
+        ip[n].ki.time = 0;
+        ip[n].ki.dwFlags = KEYEVENTF_SCANCODE; // 0 for key press
+        ip[n].ki.dwExtraInfo = 0;
+    }
+
+    SHORT mapkey = VkKeyScan(*letter);
+    int shift = (mapkey & 0x100) > 0;
+    
+    if (shift == 1) {
+        // fprintf(stderr, "** shift\n");
+        ip[c].ki.dwFlags = KEYEVENTF_SCANCODE;
+        ip[c].ki.wScan = MapVirtualKey( VK_LSHIFT, 0 );
+        c = 1;
+    }
+
+    // fprintf(stderr, "** %c\n",*letter);
+
+    ip[c].ki.wScan = MapVirtualKey( LOBYTE(mapkey), 0);
+    c++;
+    //release key
+    ip[c].ki.wScan = MapVirtualKey( LOBYTE(mapkey), 0);
+    ip[c].ki.dwFlags =  KEYEVENTF_SCANCODE | KEYEVENTF_KEYUP;
+
+    if (shift == 1) {
+        // fprintf(stderr, "** none\n");
+        ip[3].ki.dwFlags = KEYEVENTF_SCANCODE | KEYEVENTF_KEYUP;
+        ip[3].ki.wScan = MapVirtualKey( VK_LSHIFT, 0 );
+    }
+
+    if (shift == 1) {
+        SendInput(4, ip, sizeof(INPUT));
+    } else {
+        SendInput(2, ip, sizeof(INPUT));
     }
     
-    // Now we need to adjust each INPUT structure so that
-    // the whole sequence is equivalent to typing "hello".
-    // Note that every second structure must be changed
-    // from a key press to a key release.
+    return 0;
+}
+
+int SendAltTab(void) {
+    // Create an array of generic keyboard INPUT structures
+    INPUT ip[4];
+    for (int n=0; n<4; ++n) {
+        ip[n].type = INPUT_KEYBOARD;
+        ip[n].ki.wScan = 0;
+        ip[n].ki.wVk = 0;
+        ip[n].ki.time = 0;
+        ip[n].ki.dwFlags = KEYEVENTF_SCANCODE; // 0 for key press
+        ip[n].ki.dwExtraInfo = 0;
+    }
+
+    ip[0].ki.dwFlags = KEYEVENTF_SCANCODE; // 0 for key press
+    ip[0].ki.wScan = 0x38; //press alt
+
+    ip[1].ki.dwFlags = 0; // 0 for key press
+    ip[1].ki.wVk = VK_TAB;; //press tab
+
+    ip[2].ki.dwFlags = KEYEVENTF_KEYUP;
+    ip[2].ki.wVk = VK_TAB; //release tab
     
-    // INPUT structures to press and release the 'H' key
-    ip[0].ki.wVk = 'H';
-    ip[1].ki.wVk = 'H';
-    ip[1].ki.dwFlags = KEYEVENTF_KEYUP;
-    // INPUT structures to press and release the 'E' key
-    ip[2].ki.wVk = 'E';
-    ip[3].ki.wVk = 'E';
-    ip[3].ki.dwFlags = KEYEVENTF_KEYUP;
-    // INPUT structures to press and release the 'L' key
-    ip[4].ki.wVk = 'L';
-    ip[5].ki.wVk = 'L';
-    ip[5].ki.dwFlags = KEYEVENTF_KEYUP;
-    // INPUT structures to press and release the 'L' key
-    ip[6].ki.wVk = 'L';
-    ip[7].ki.wVk = 'L';
-    ip[7].ki.dwFlags = KEYEVENTF_KEYUP;
-    // INPUT structures to press and release the 'O' key
-    ip[8].ki.wVk = 'O';
-    ip[9].ki.wVk = 'O'; 
-    ip[9].ki.dwFlags = KEYEVENTF_KEYUP;
-    
-    // Send all 10 INPUT structures to type the word
-    SendInput(10, ip, sizeof(INPUT));
-     
-    // The End
+    ip[3].ki.wScan = 0x38;
+    ip[3].ki.dwFlags = KEYEVENTF_SCANCODE | KEYEVENTF_KEYUP;
+
+    SendInput(4, ip, sizeof(INPUT));
+
     return 0;
 }
